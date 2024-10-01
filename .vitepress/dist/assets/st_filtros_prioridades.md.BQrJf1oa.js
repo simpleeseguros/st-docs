3502da30e7b3f20a67bdb668917bd38c9e195459
Alberbackdev <albertograterolca@gmail.com>
2024-10-01 17:15:00 -0400

    serve json

--- .vitepress/dist/assets/st_filtros_prioridades.md.BQrJf1oa.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as r}from"./chunks/framework.C5Pb2s_8.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"st/filtros/prioridades.md","filePath":"st/filtros/prioridades.md"}'),s={name:"st/filtros/prioridades.md"};function a(o,i,d,c,n,p){return r(),e("div")}const l=t(s,[["render",a]]);export{_ as __pageData,l as default};
