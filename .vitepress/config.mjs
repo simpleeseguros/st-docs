bb191848e071b3b8bee94b1b334919d98ea17c2e
Alberto <albertograterolca@gmail.com>
2024-11-24 23:44:40 -0400

    dist

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  
   lang: "es-ES",
   title: " ",
   description: "Documentación ST",
